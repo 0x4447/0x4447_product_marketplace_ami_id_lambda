3f969f10924c112e52b79438c3948a9a8694edcb
David Gatti <github@gatti.io>
2020-09-19 19:49:57 +0200

    Added filtering for only public AMIs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let AWS = require("aws-sdk");
 
 //
-//	This Lambda will get the AMI ID of a AWS Marketplace product for each 
+//	This Lambda will get the AMI ID of a AWS Marketplace product for each
 //	selected region.
 //
 exports.handler = (event) => {
@@ -20,9 +20,13 @@ exports.handler = (event) => {
 			//	This array will keep all the prosmies to get the date
 			//	from all the regions that our AMI is in.
 			//
-			promises: [],
+            promises: [],
+            //
+            //  Holds the raw result from AWS, which we have to clean up.
+            //
+            mixed_amis: null,
 			//
-			//	This array will hold the resoult of the previous query 
+			//	This array will hold the resoult of the previous query
 			//	with tall the IDs that we've got back with the region.
 			//
 			ami_ids: [],
@@ -35,10 +39,14 @@ exports.handler = (event) => {
 		//
 		//	->	Start the chain.
 		//
-		prepare_the_query(container)
+		execute_the_queries(container)
 			.then(function(container) {
 
-				return execute_all_the_promises(container);
+				return collect_all_the_promises(container);
+
+			}).then(function(container) {
+
+				return pick_the_public_ones(container);
 
 			}).then(function(container) {
 
@@ -75,11 +83,11 @@ exports.handler = (event) => {
 //	of promises with all the individual queris to AWS, so they can be executed
 //	at the same time.
 //
-function prepare_the_query(container)
+function execute_the_queries(container)
 {
 	return new Promise(function(resolve, reject) {
 
-        console.info("prepare_the_query");
+        console.info("execute_the_queries");
 
 		//
 		//	1.	Make the variable shorter.
@@ -109,11 +117,11 @@ function prepare_the_query(container)
 //
 //	Once we have all the queries prepared, we can execute them in on go.
 //
-function execute_all_the_promises(container)
+function collect_all_the_promises(container)
 {
 	return new Promise(function(resolve, reject) {
 
-		console.info("execute_all_the_promises");
+		console.info("collect_all_the_promises");
 
 		//
 		//	1.	Exectue all the prosmies.
@@ -122,28 +130,74 @@ function execute_all_the_promises(container)
 			.then(function(values) {
 
 				//
-				//	1.	Loop over the result and just extract and keep the
-				//		AMI IDs, which we care only about.
+                //	1.	Save all that we get as is, and we sort the result
+                //      later.
 				//
-				values.forEach(function(value) {
-					
-					container.ami_ids.push({
-						region: value.region,
-						ami_id: value.ami_id[0].ImageId
-					});
-
-				});
+				container.mixed_amis = values;
 
 				//
 				//	->	Move to the next promise.
 				//
 				return resolve(container);
-		
+
 			}).catch(function(error){
 
 				return reject(error);
 
-			});		
+			});
+
+	});
+}
+
+//
+//  AWS will send back pubnlic and private AMI IDs, and we only can use the
+//  public ones. For this we have to filter out the private ones and keep
+//  the public ones.
+//
+function pick_the_public_ones(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		console.info("pick_the_public_ones");
+
+        //
+        //  1.  Loop over all the data to find what we need.
+        //
+        container.mixed_amis.forEach(function(data) {
+
+            //
+            //  1.  First we save the region for the active AMI
+            //
+            let region = data.region;
+
+            //
+            //  2.  Then we loop across all the AMIs that we got back to find
+            //      the public one.
+            //
+            data.amis.forEach(function(ami) {
+
+                //
+                //  1.  Be on high alert for the Public AMI - since this is what
+                //      we are after.
+                //
+                if(ami.Public === true)
+                {
+                    //
+                    //  1.  Once we have it all we put it together in to a nice
+                    //      package.
+                    //
+                    container.ami_ids.push({
+                        region: region,
+                        ami_id: ami.ImageId
+                    });
+                }
+            })
+        });
+
+		//
+        //	->	Move to the next promise.
+        //
+        return resolve(container);
 
 	});
 }
@@ -181,7 +235,7 @@ function formatting(container)
 }
 
 //
-//	This is a standalone Promise, used to make individual queris to 
+//	This is a standalone Promise, used to make individual queris to
 //	AWS using Promise All.
 //
 function query(region, id)
@@ -191,15 +245,15 @@ function query(region, id)
         console.info("query - " + region);
 
 		//
-		//	1.	We are initialzign the class over and over again on 
-		//		pourpes, becase we need to make queris to all the 
+		//	1.	We are initialzign the class over and over again on
+		//		pourpes, becase we need to make queris to all the
 		//		individual regions that our product is in.
 		//
 		let ec2 = new AWS.EC2({
 			apiVersion: '2016-11-15',
 			region: region
 		});
-		
+
 		//
 		//	2.	Prepare the query.
 		//
@@ -229,14 +283,14 @@ function query(region, id)
 			{
 				console.info(params);
 				return reject(error);
-			} 
+            }
 
 			//
 			//	->	Move to the next promise.
 			//
 			return resolve({
 				region: region,
-				ami_id: data.Images
+				amis: data.Images
 			});
 
 		});
